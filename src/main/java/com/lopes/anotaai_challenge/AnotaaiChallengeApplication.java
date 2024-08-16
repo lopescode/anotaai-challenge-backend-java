907c935c408a6e72fe734393391b419d0dcb6b14
Lopes <leonardolopesb97@hotmail.com>
2024-08-16 00:09:43 -0300

    Java setup initialized

---
+++ src/main/java/com/lopes/anotaai_challenge/AnotaaiChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.lopes.anotaai_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotaaiChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotaaiChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lopes/anotaai_challenge/AnotaaiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopes.anotaai_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotaaiChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
