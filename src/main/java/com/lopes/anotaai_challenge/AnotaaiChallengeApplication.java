be8f49f6a3fcec013a275816c4d9a7adbb27ee6c
Lopes <leonardolopesb97@hotmail.com>
2024-08-16 00:17:28 -0300

    feat: Setup mongo

--- src/main/java/com/lopes/anotaai_challenge/AnotaaiChallengeApplication.java
+++ src/main/java/com/lopes/anotaai_challenge/AnotaaiChallengeApplication.java
@@ -2,12 +2,12 @@ package com.lopes.anotaai_challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class AnotaaiChallengeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AnotaaiChallengeApplication.class, args);
 	}
-
 }
