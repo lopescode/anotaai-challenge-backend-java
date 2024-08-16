e5edb75166e4b370584096a403a871a2fb15204a
Lopes <leonardolopesb97@hotmail.com>
2024-08-16 00:23:03 -0300

    feat: Product and Category entities creation

---
+++ src/main/java/com/lopes/anotaai_challenge/domain/product/Product.java
@@ -0,0 +1,22 @@
+package com.lopes.anotaai_challenge.domain.product;
+
+import com.lopes.anotaai_challenge.domain.category.Category;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private String ownerId;
+    private Integer price; // All prices are in cents
+    private Category category;
+}
